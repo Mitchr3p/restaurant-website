fe107cfdca2fdc9a693aaa4a9e4c73aabdbad740
Mitchell <mitchellr3p@gmail.com>
2023-12-23 23:06:31 +0100

    add code

--- app.js
+++ app.js
@@ -5,4 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleBtn.addEventListener("click", function () {
     dropdownMenu.classList.toggle("open");
   });
+
+  const liItems = document.querySelectorAll('ul li');
+  const imgItems = document.querySelectorAll('.product p');
+
+  liItems.forEach(li => {
+    li.addEventListener('click', function () {
+      // Toggle "active" class
+      liItems.forEach(item => item.classList.remove('active'));
+      this.classList.add('active');
+
+      // Filter images
+      const filterValue = this.textContent.toLowerCase();
+      imgItems.forEach(img => {
+        const imgFilter = img.getAttribute('data-filter').toLowerCase();
+        img.style.display = (filterValue === 'all menu' || imgFilter === filterValue) ? 'block' : 'none';
+      });
+    });
+  });
 });
